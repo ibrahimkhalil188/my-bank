5da5ade08608d310a8ec900ecb2a0706c09c7979
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-11 07:07:26 +0600

    inter acitvity done

--- banking.js
+++ banking.js
@@ -3,15 +3,47 @@ document.getElementById("deposit-button").addEventListener('click', function () 
     const depositInputField = document.getElementById("deposit-input");
 
     const depositInput = depositInputField.value;
+    if (depositInputField.value <= 0) {
+        alert("Only positive amount accepted")
+    }
 
+    else {
+        const previousDeposit = document.getElementById("previous-deposit")
 
-    const previousDeposit = document.getElementById("previous-deposit")
+        const previousDepositAmount = previousDeposit.innerText
 
-    const previousDepositAmount = previousDeposit.innerText
+        const newDepositAmont = parseFloat(previousDepositAmount) + parseFloat(depositInput)
 
-    const newDepositAmont = parseFloat(previousDepositAmount) + parseFloat(depositInput)
+        previousDeposit.innerText = newDepositAmont;
 
-    previousDeposit.innerText = newDepositAmont;
+        // update balance
+
+        const balance = document.getElementById("balance");
+
+        const previousBalance = balance.innerText;
+
+        const newbalance = parseFloat(previousBalance) + parseFloat(depositInput)
+
+        balance.innerText = newbalance;
+
+        depositInputField.value = ""
+
+    }
+})
+
+document.getElementById("withdraw-button").addEventListener('click', function () {
+
+    const withdrawInputField = document.getElementById("withdraw-input");
+
+    const withdrawInputAmount = withdrawInputField.value;
+
+    const previousWithdraw = document.getElementById("previous-withdraw");
+
+    const previousWithdrawAmout = previousWithdraw.innerText;
+
+    const newWithdrawAmount = parseFloat(withdrawInputAmount) + parseFloat(previousWithdrawAmout)
+
+    previousWithdraw.innerText = newWithdrawAmount;
 
     // update balance
 
@@ -19,10 +51,10 @@ document.getElementById("deposit-button").addEventListener('click', function () 
 
     const previousBalance = balance.innerText;
 
-    const newbalance = parseFloat(previousBalance) + parseFloat(depositInput)
+    const newBalaceAfterWithdraw = parseFloat(previousBalance) - parseFloat(withdrawInputAmount)
 
-    balance.innerText = newbalance;
+    balance.innerText = newBalaceAfterWithdraw;
 
-    depositInputField.value = ""
+    withdrawInputField.value = ''
 
 })
